5cb5f346c5443e954ec1eb2454805b23ca5f570c
juniorfelix998 <juniorfelix998@gmail.com>
2021-07-18 13:32:30 +0300

    added more controllers

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,3 +1,5 @@
+import {json} from "express";
+
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require("crypto");
@@ -31,9 +33,182 @@ const register = async (data,role,res) =>{
         });
 
     }catch (err) {
-        
+        return res.status(500).json({
+            message: err.message,
+            success: false,
+        })
     }
 };
+
+const login = async (data, res) => {
+    try {
+        let {email, password} = data;
+        const user = await User.findOne({email: email});
+
+        if(!user){
+            return res.status(404).send({
+                message:'The user not found',
+                email:'Incorrect Email!',
+                success:false});
+        }
+        let isMatch = await bcrypt.compare(password,user.password);
+        if(user && isMatch){
+            let token = jwt.sign({
+                user_id: user._id,
+                role:user.role,
+                email:user.email,
+                name:user.name
+            },process.env.JWT_SECRET,{
+                expiresIn: '7 days',
+            });
+            let profile = {
+                email:user.email,
+                role:user.role,
+                name:user.name
+            };
+
+            let result = {
+                user:profile,
+                token:token,
+                expiresIn:168,
+            };
+
+            return res.status(200).json({
+                ...result,
+                message:"Login successful",
+                success: true
+            });
+        }else{
+            return res.status(403).json({
+                message:"Failed login attempt",
+                email:"Incorrect Password",
+                success:false
+
+            })
+        }
+    }catch (err) {
+        return res.status(500).json({
+            message: err.message,
+            success: false
+        })
+
+    }
+
+}
+
+const verify = async (data,res) => {
+    try{
+        let {code} = data;
+        const user = await User.findOne({verificationCode:code});
+        if(!user) {
+            return res.status(404).json({
+                message: "Invalid code",
+                success: false
+            });
+        } else if(user.isEmailVerified) {
+            return res.status(404).json({
+                message: "Email already verified",
+                success: false
+            });
+        }
+        await user.update({isEmailVerified:true});
+        return res.status(201).json({
+            message: "Email verification success",
+            success: true
+        });
+    }catch (err) {
+        return res.status(500).json({
+            message: err.message,
+            success: false
+        })
+    }
+};
+
+const forgotPassword = async (data,res) => {
+    try{
+        let {email} = data;
+        const user = await User.findOne( { email: email });
+        if(!user) {
+            return res.status(404).json({
+                message: "Invalid email",
+                success: false
+            });
+        }
+        const code = crypto.randomInt(100000, 1000000);
+        const passwordResetCode = await bcrypt.hash(code.toString(), 16);
+        await user.update({passwordResetCode : passwordResetCode});
+        // forgotPasswordSender(user.email, user.name, code);
+        return res.status(404).json({
+            message: "Verification code sent to your email",
+            success: true,
+        });
+    }catch (err) {
+        return res.status(500).json({
+            message: err.message,
+            success: false
+        })
+    }
+};
+
+const resetPassword = async (data, res) => {
+    try {
+        console.log(data)
+        let { email, code, newPassword } = data;
+        const user = await User.findOne( { email: email });
+        if(!user) {
+            return res.status(404).json({
+                message: "Invalid email",
+                success: false
+            });
+        }
+        let isMatch = await bcrypt.compare(code.toString(), user.passwordResetCode);
+        if(isMatch) {
+            const hashedPassword = await bcrypt.hash(newPassword, 16);
+            await user.update({password: hashedPassword}, {passwordResetCode: ""});
+            return res.status(201).json({
+                message: "Your password has been successfully reset",
+                success: true
+            });
+        } else {
+            return res.status(404).json({
+                message: "Invalid code",
+                success: false
+            });
+        }
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message,
+            success: false
+        })
+    }
+};
+
+const changePassword = async (req, res) => {
+    try {
+        let { oldPassword, newPassword } = req.body;
+        const user = await User.findById(req.user._id);
+        let isMatch = await bcrypt.compare(oldPassword, user.password);
+        if(isMatch) {
+            const hashedPassword = await bcrypt.hash(newPassword, 16);
+            await user.update({password: hashedPassword});
+            return res.status(201).json({
+                message: "Your password has been successfully reset",
+                success: true
+            });
+        } else {
+            return res.status(404).json({
+                message: "Your old password is incorrect",
+                success: false
+            });
+        }
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message,
+            success: false
+        })
+    }
+};
+
 
 const validateEmail = async (email) =>{
     let user = await User.findOne({email: email});
@@ -43,4 +218,13 @@ const validateEmail = async (email) =>{
         return false;
     }
 
+};
+
+module.exports = {
+    login,
+    register,
+    verify,
+    forgotPassword,
+    resetPassword,
+    changePassword
 };
